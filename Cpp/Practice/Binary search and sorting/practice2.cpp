16f8a508c889d1d47838aa9f63cb966d20ac9b4c
Ravi Yadav <148179385+cipherravi@users.noreply.github.com>
2025-05-27 11:32:25 +0530

    DAY 8

--- Cpp/Practice/Binary search and sorting/practice2.cpp
+++ Cpp/Practice/Binary search and sorting/practice2.cpp
@@ -43,42 +43,91 @@ int main()
 
     //*****************************// DIVIDE TWO NUMBERS USING BINARY SEARCH
 
-    int dividend = 21;
-    int divisor = 22;
+    // int dividend = 21;
+    // int divisor = 22;
+    // int start = 0;
+    // int end = abs(dividend);
+    // int mid;
+    // int result = 0;
+
+    // while (start <= end)
+    // {
+    //     mid = start + (end - start) / 2;
+    //     if (abs(mid * divisor) == abs(dividend))
+    //     {
+    //         result = mid;
+    //         break;
+    //     }
+    //     else if (abs(mid * divisor) > abs(dividend))
+    //     {
+    //         end = mid - 1;
+    //     }
+    //     else
+    //     {
+    //         result = mid;
+    //         start = mid + 1;
+    //     }
+    // }
+
+    // if ((dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0))
+    // {
+    //     result = result;
+    // }
+    // else
+    // {
+    //     result = -result;
+    // }
+
+    // cout << "Division : " << dividend << "/" << divisor << " = " << result << endl;
+
+    /***************************/ // FIND ODD OCCURING ELEMENT IN AN ARRAY using binary search
+
+    vector<int> arr{1, 1, 2, 2, 3, 3, 4, 4, 3, 5, 5, 100, 100, 4, 4};
+    int N = arr.size();
     int start = 0;
-    int end = abs(dividend);
+    int end = N - 1;
     int mid;
-    int result = 0;
+    // cout << N << end;
+    int ans = -1;
 
     while (start <= end)
     {
+
         mid = start + (end - start) / 2;
-        if (abs(mid * divisor) == abs(dividend))
+        if (start == end)
         {
-            result = mid;
+            ans = arr[start];
             break;
         }
-        else if (abs(mid * divisor) > abs(dividend))
+        else if (mid % 2 == 0)
         {
-            end = mid - 1;
+            if (arr[mid] == arr[mid + 1])
+            {
+                cout << "inside even if" << endl;
+                cout << mid << endl;
+                start = mid + 2;
+            }
+            else
+            {
+
+                end = mid;
+            }
         }
         else
         {
-            result = mid;
-            start = mid + 1;
+
+            if (arr[mid] == arr[mid - 1])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
         }
     }
 
-    if ((dividend < 0 && divisor < 0) || (dividend > 0 && divisor > 0))
-    {
-        result = result;
-    }
-    else
-    {
-        result = -result;
-    }
-
-    cout << "Division : " << dividend << "/" << divisor << " = " << result << endl;
+    cout << "Odd occuring element is : " << ans << endl;
 
     return 0;
 }
